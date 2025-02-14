845d650f7f672ec4390768dd7d842c488ee8d64c
Anand V <42205127+vanand46@users.noreply.github.com>
2025-02-15 00:28:49 +0530

    Update main.tf

--- provisioners_test/main.tf
+++ provisioners_test/main.tf
@@ -6,8 +6,8 @@ locals {
 
 # Provider section
 provider "aws" {
-  access_key = "YOUR_AWS_ACCESS_KEY"
-  secret_key = "YOUR_AWS_SECRET_KEY"
+  access_key = "adf"
+  secret_key = "dfd/asdfgdfg"
   region = "us-east-1"
 }
 
